41369d4a07c049e7fa38cba550527e0ef1462e59
Raivo Kinne <raivokinne236@gmail.com>
2023-09-29 21:22:21 +0300

    fixing css

--- src/components/Stundas.tsx
+++ src/components/Stundas.tsx
@@ -14,23 +14,28 @@ function Stundas({
   }
   return (
     <main>
-      <p className="flex justify-center items-center text-xl"> {week(index)}</p>
+      <p className="flex justify-center items-center text-2xl">
+        {" "}
+        {week(index)}
+      </p>
       <section className="flex justify-center items-center w-full my-8">
         <table className="w-[450px]  border-collapse border border-gray-300 shadow-md table-fixed md:w-[600px] lg:w-[900px] xl:w-[1200px]">
           <thead>
             <tr>
-              <th className="p-3 text-left bg-gray-200">{dayOfWeek(index)}</th>
+              <th className="p-3  bg-gray-200 text-center">
+                {dayOfWeek(index)}
+              </th>
             </tr>
           </thead>
           <tbody className="">
             {classes.map((klase, index) => (
               <tr key={index}>
-                <td className="flex w-full">
-                  <p className="p-3 border border-gray-300 flex justify-between w-full items-center text-left">
+                <td className="grid md:flex w-full">
+                  <p className="p-3 border border-gray-300 flex md:justify-normal justify-center w-full items-center text-left">
                     {index + 1 + ". " + klase}
                   </p>
 
-                  <p className="p-3 border border-gray-300 flex justify-between w-[250px] items-center">
+                  <p className="p-3 border border-gray-300 flex justify-center w-full md:w-[300px] items-center ">
                     {teacher[index]}
                   </p>
                 </td>
