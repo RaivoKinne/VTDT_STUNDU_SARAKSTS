2c71c3696f0e30ce2c9347ca95690285d1d9610e
Raivo Kinne <raivokinne236@gmail.com>
2023-10-02 22:46:46 +0300

    fixing mobile border

--- src/components/stundas/Stundas.tsx
+++ src/components/stundas/Stundas.tsx
@@ -10,12 +10,12 @@ export default function Stundas({
   return (
     <>
       <tr key={index}>
-        <td className="grid w-full md:flex">
-          <div className="p-3 border border-black flex md:justify-normal justify-center w-full h-[60px] items-center text-left">
+        <td className="grid w-full md:flex border-2 border-black">
+          <div className="p-3 flex border-b-2 border-black md:border-none md:justify-normal justify-center w-full h-[60px] items-center text-left">
             {index + 1 + ". " + klase}
           </div>
 
-          <div className="p-3 border border-black flex flex-wrap justify-center w-full md:w-[300px] h-[60px] items-center ">
+          <div className="p-3  md:border-l-4 md:border-black flex flex-wrap justify-center w-full md:w-[300px] h-[60px] items-center ">
             {teacher[index]}
           </div>
         </td>
